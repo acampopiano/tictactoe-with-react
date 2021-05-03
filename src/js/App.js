b9ef0875d43f08fe536f492730be4d993329fb4f
Andres Campopiano <acampopiano@gmail.com>
2021-05-03 01:52:13 +0000

    subiendo tictactoe no finalizado

--- src/js/App.js
+++ src/js/App.js
@@ -13,7 +13,54 @@ const App = () => {
     const [winner, setWinner] = useState("");
 
     const updateBoard=(loc, player)=>{
-        
+        if(gameBoard[loc] === 'X' || gameBoard[loc] === 'O') return; //invalid move
+        let currentGameBoard = gameBoard;
+        currentGameBoard.splice(loc,1,turn);
+        setGameBoard({gameBoard:currentGameBoard});
+        let topRow = gameBoard[0] + gameBoard[1] + gameBoard[2];
+        if(topRow.match(/XXX|OOO/)){
+            setWinner({winner:turn});
+            return;
+        }
+        let middleRow = gameBoard[3] + gameBoard[4] + gameBoard[5];
+        if(middleRow.match(/XXX|OOO/)){
+            setWinner({winner:turn});
+            return;
+        }
+        let bottomRow = gameBoard[6] + gameBoard[7] + gameBoard[8];
+        if(bottomRow.match(/XXX|OOO/)){
+            setWinner({winner:turn});
+            return;
+        }
+        let leftColumn = gameBoard[0] + gameBoard[3] + gameBoard[6];
+        if(leftColumn.match(/XXX|OOO/)){
+            setWinner({winner:turn});
+            return;
+        }
+        let middleColumn = gameBoard[1] + gameBoard[4] + gameBoard[7];
+        if(middleColumn.match(/XXX|OOO/)){
+            setWinner({winner:turn});
+            return;
+        }
+        let rightColumn = gameBoard[2] + gameBoard[5] + gameBoard[8];
+        if(rightColumn.match(/XXX|OOO/)){
+            setWinner({winner:turn});
+            return;
+        }
+         let leftDiag = gameBoard[0] + gameBoard[4] + gameBoard[8];
+        if(leftDiag.match(/XXX|OOO/)){
+            setWinner({winner:turn});
+            return;
+        }
+         let rightDiag = gameBoard[2] + gameBoard[4] + gameBoard[6];
+        if(rightDiag.match(/XXX|OOO/)){
+            setWinner({winner:turn});
+            return;
+        }
+        let moves = gameBoard.join('').replace(/ /g,'');
+        if(moves.length === 9) setWinner({winner:'d'}) ;
+        setTurn({turn: (turn === 'X') ? 'O':'X'})
+
     }  
     const resetBoard= ()=>{
 
